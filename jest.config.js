bc32c5e670e1f5c4a8ca87e004abca82119bc15d
A90-svg <bts.kth23@gmail.com>
2025-12-18 10:49:41 +0300

    sast report

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,6 @@
 export default {
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.js'],
-  globals: {
-    'ts-jest': {
-      useESM: true
-    }
-  },
   transform: {},
   moduleNameMapping: {
     '^(\\.{1,2}/.*)\\.js$': '$1'
